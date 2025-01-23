4170c5738fc5d4fb150d367c9ca53c5bd84bf277
soham chogale <chogalesoham74@gmail.com>
2025-01-23 20:51:10 +0530

    feat/add get single post api

--- backend/Routes/post-router.js
+++ backend/Routes/post-router.js
@@ -3,10 +3,13 @@ const { CloudinaryFileUploder } = require("../Middlwares/file-uploder");
 const {
   CreateNewPost,
   GetAllPost,
+  GetSinglePost,
 } = require("../Controllers/post-controllers");
 
 Router.post("/", CloudinaryFileUploder.single("postImage"), CreateNewPost);
 
 Router.get("/", GetAllPost);
+
+Router.get("/:id", GetSinglePost);
 
 module.exports = Router;
